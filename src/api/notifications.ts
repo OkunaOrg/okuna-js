450cf697240dd3eacbbbf946615bdbfec46b177b
József Sallai <jozsef@sallai.me>
2019-08-18 02:36:18 +0300

    :pencil: add better docs for notifications and userinvites

--- src/api/notifications.ts
+++ src/api/notifications.ts
@@ -1,11 +1,19 @@
 import { APIRequest } from "../utils/APIRequest";
 import { RequestOpts, LimitationParams } from '../typings';
 
+/**
+ * @api public
+ * NotificationsAPI - Provides methods to interact with the Notifications API
+ */
 class NotificationsAPI extends APIRequest {
   constructor(opts: RequestOpts) {
     super(opts);
   }
 
+  /**
+   * `GET /api/notifications/` - promises to retrieve the user's notifications
+   * @param {LimitationParams} opts - The limitation rules
+   */
   async getNotifications(opts: LimitationParams) {
     if (opts.maxId) {
       this._params.maxId = opts.maxId;
@@ -18,28 +26,47 @@ class NotificationsAPI extends APIRequest {
     return this.get();
   }
 
+  /**
+   * `POST /api/notifications/read/` - promises to mark notifications as read
+   * @param {LimitationParams} opts - The limitation rules
+   */
   async readNotifications(opts: LimitationParams) {
     this._paths.push('read');
 
     return this.post(opts);
   }
 
+  /**
+   * `DELETE /api/notifications/` - promises to delete all notifications
+   */
   async deleteNotifications() {
     return this.delete();
   }
 
+  /**
+   * `DELETE /api/notifications/:id/` - promises to delete a notification
+   * @param {number} id - The ID of the notification
+   */
   async deleteNotification(id: number) {
     this._paths.push(id.toString());
 
     return this.delete();
   }
 
+  /**
+   * `GET /api/notifications/:id/` - promises to get a notification
+   * @param {number} id - The ID of the notification
+   */
   async getNotification(id: number) {
     this._paths.push(id.toString());
 
     return this.get();
   }
 
+  /**
+   * `POST /api/notifications/:id/` - promises to mark a notification as read
+   * @param {number} id - The ID of the notification
+   */
   async readNotification(id: number) {
     this._paths.push(id.toString());
 
--- src/api/userInvites.ts
+++ src/api/userInvites.ts
@@ -1,11 +1,19 @@
 import { APIRequest } from '../utils/APIRequest';
 import { RequestOpts } from '../typings';
 
+/**
+ * @api public
+ * UserInvitesAPI - Provides methods to interact with the User Invites API
+ */
 class UserInvitesAPI extends APIRequest {
   constructor(opts: RequestOpts) {
     super(opts);
   }
 
+  /**
+   * `PUT /api/invites/` - promises to create an invite
+   * @param {string} nickname - A nickname for the notification
+   */
   async create(nickname: string) {
     const body = {};
 
@@ -16,12 +24,23 @@ class UserInvitesAPI extends APIRequest {
     return this.putFormdata(body);
   }
 
+  /**
+   * `PATCH /api/invites/` - promises to update a notification's nickname
+   * @param {string} id - The ID of the notification
+   * @param {string} nickname - The new nickname
+   */
   async update(id: number, nickname: string) {
     this._paths.push(id.toString());
 
     return this.patchFormdata({ nickname });
   }
 
+  /**
+   * `GET /api/invites/` - promises to retrieve a list of all invites
+   * @param {number} offset
+   * @param {number} count
+   * @param {boolean} pending
+   */
   async getUserInvites(
     offset: number | null = null,
     count: number | null = null,
@@ -42,6 +61,12 @@ class UserInvitesAPI extends APIRequest {
     return this.get();
   }
 
+  /**
+   * `GET /api/invites/search/` - promises to search between the user invites
+   * @param {string} query
+   * @param {number} count
+   * @param {boolean} pending
+   */
   async searchUserInvite(
     query: string,
     count: number | null = null,
@@ -61,12 +86,21 @@ class UserInvitesAPI extends APIRequest {
     return this.get();
   }
 
+  /**
+   * `DELETE /api/invites/:id/` - promises to delete an invite
+   * @param {number} id - The ID of the invite
+   */
   async deleteUserInvite(id: number) {
     this._paths.push(id.toString());
 
     return this.delete();
   }
 
+  /**
+   * `POST /api/invites/:id/email/` - promises to sent an email with the invitation
+   * @param {number} id - The ID of the invite
+   * @param {string} email - The email to which the invite will be sent
+   */
   async email(id: number, email: string) {
     this._paths.push(id.toString(), 'email');
 
